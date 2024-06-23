b52f9c754520fee993fecd2c30c7d7f27629c04f
Sim Yu Bin Brayden <braydensim00@gmail.com>
2024-06-23 11:10:09 +0800

    undo mongodb

--- main.py
+++ main.py
@@ -18,37 +18,6 @@ import matplotlib.pyplot as plt
 #
 #st.set_page_config(layout="wide")
 #login
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-uri = "mongodb+srv://braydensim00:1FxyjAPHrAjnjcUw@cloudhacks24.vcsrt7m.mongodb.net/?retryWrites=true&w=majority&appName=cloudhacks24"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    st.write("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    st.write(e)
-@st.cache_resource
-def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
-
-client = init_connection()
-
-# Pull data from the collection.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def get_data():
-    db = client.mydb
-    items = db.mycollection.find()
-    items = list(items)  # make hashable for st.cache_data
-    return items
-
-items = get_data()
-
-# Print results.
-for item in items:
-    st.write(f"{item['name']} has a :{item['pet']}:")
 
 # Configure the Gemini API
 genai.configure(api_key="AIzaSyAZ7myOXP5C5GS4wOq5X4yTstZ2ttH5eos")
